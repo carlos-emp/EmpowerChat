26315c0feac8afebf2896e6a4bf9d904c5023a42
carlos-emp <carlos.vazquez@empowerlabs.com>
2015-06-19 17:49:55 -0500

    Actualizar usuario

--- js/admin.js
+++ js/admin.js
@@ -24,84 +24,100 @@
     });
 
 
-  
+
 
 
     /*obtengo todos los datos necesarios para el administrador*/
     module.controller('initializeadmin', function($scope, $data, $http) {
 
-      
-       
+
+
 
 
 
         $scope.register = function(){
             registerUSer();
         }
-        
+
         $scope.Logout=function(){
             Logout();
         }
-       
-       
+
+
         /*obtengo los datos de los usuarios*/
         var myDataRef = new Firebase('https://userempowerlabs.firebaseio.com');
-            
-           
-            myDataRef.on('child_added', function(snapshot) {
-                var user = snapshot.val();
-                var addUser = new Object();
-                addUser.name=user.name;
-                addUser.Password=user.password;
-                addUser.id=user.id;
-                addUser.email=user.email;
-                addUser.rol=user.rol;
-                listOfUsers.push(addUser);
-                displayChatMessage(user.email,user.id,user.name, user.password, user.rol);
-            });
+
+
+        myDataRef.on('child_added', function(snapshot) {
+            var user = snapshot.val();
+            var addUser = new Object();
+            addUser.name=user.name;
+            addUser.Password=user.password;
+            addUser.id=user.id;
+            addUser.email=user.email;
+            addUser.rol=user.rol;
+            listOfUsers.push(addUser);
+            displayChatMessage(user.email,user.id,user.name, user.password, user.rol);
+        });
 
     });
+
+
+    function UpdateUser(user){
+        var myDataRef = new Firebase('https://userempowerlabs.firebaseio.com'+user);
+        var email = $('#emailInput').val().trim();
+        var name = $('#nameInput').val().trim();
+        var password = $('#passwordInput').val().trim();
+        
+        if(validateEmail(email)){
+            if(name.length>0){
+                if(validate)
+            }
+        }
+    }
 
     /*funcion para el registro de un nuevo usuario*/
     function registerUSer(){
         var myDataRef = new Firebase('https://userempowerlabs.firebaseio.com');
-                var email = $('#emailInput').val().trim();
-                var name = $('#nameInput').val().trim();
-                var role = $('#roleInput').val().trim();
-                var password = $('#passwordInput').val().trim();
-                /*obtengo todos los valores*/
-                
-                if(validateEmail(email))
+        var email = $('#emailInput').val().trim();
+        var name = $('#nameInput').val().trim();
+        var role = $('#roleInput').val().trim();
+        var password = $('#passwordInput').val().trim();
+        /*obtengo todos los valores*/
+
+        if(validateEmail(email))
+        {
+            if(password.length>0)
+            {
+                if(name.length>0)
                 {
-                    if(password.length>0)
+                    if(validateUseEmail(email))
                     {
-                        if(name.length>0)
-                        {
-                            if(validateUseEmail(email))
-                            {
-                                $(".error").addClass("hide");
-                                var id = idNuevo+1
-                                myDataRef.push({email: email, id:id, name: name, password: password, rol: role});
-                                $('#messageInput').val('');
-                                showMessage("Usuario registrado correctamente");
-                                $('#emailInput').val("");
-                                $('#nameInput').val("");
-                                $('#roleInput').val("Seleccione Rol");
-                                $('#passwordInput').val("");
-                            }else{
-                                 showMessage("El correo ya fue utilizado anteriormente");
-                            }
-                        }else{
-                            showMessage("Necesita proporcionar el nombre del usuario");
-                        }
+                        $(".error").addClass("hide");
+                        var id = idNuevo+1
+                        myDataRef.push({email: email, id:id, name: name, password: password, rol: role});
+                        $('#messageInput').val('');
+                        showMessage("Usuario registrado correctamente");
+                        $('#emailInput').val("");
+                        $('#nameInput').val("");
+                        $('#roleInput').val("Seleccione Rol");
+                        $('#passwordInput').val("");
                     }else{
-                        showMessage("Necesita proporcionar una contraseña");
+                        showMessage("El correo ya fue utilizado anteriormente");
                     }
-                }else {
-                    showMessage("Correo inválido");
+                }else{
+                    showMessage("Necesita proporcionar el nombre del usuario");
                 }
+            }else{
+                showMessage("Necesita proporcionar una contraseña");
+            }
+        }else {
+            showMessage("Correo inválido");
+        }
 
     }
+
+
     /*funcion para mostrar mensaje*/
     function showMessage(message)
     {
@@ -125,39 +141,39 @@
         return validate;
     }
 
-     //funcion para validar el correo
+    //funcion para validar el correo
     function validateEmail( email ) {
-    var expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    if ( !expr.test(email) )
+        var expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if ( !expr.test(email) )
         {return false;}
-      return true;
+        return true;
     }
 
 
 
 
-            /*función para mostrar los usuarios dados de alta*/
-            function displayChatMessage(email, id ,name, password, rol) {
+    /*función para mostrar los usuarios dados de alta*/
+    function displayChatMessage(email, id ,name, password, rol) {
 
 
-                contacto+='<div class="col-xs-12 col-sm-6 col-md-4"><div class="fondoAzul colorBlanco" align="center">'+
-                    email+
-                    '</div>'+
-                    '<div class="fondoBlanco padding-min"><b>ID:</b> ' +
-                    id+'<br><b>Nombre:</b> '+name+'<br><b>Rol: </b>'+rol+
-                    '</div><br></div>';
+        contacto+='<div class="col-xs-12 col-sm-6 col-md-4"><div class="fondoAzul colorBlanco" align="center">'+
+            email+
+            '</div>'+
+            '<div class="fondoBlanco padding-min"><b>ID:</b> ' +
+            id+'<br><b>Nombre:</b> '+name+'<br><b>Rol: </b>'+rol+
+            '</div><br></div>';
 
 
 
 
-                $('#usersDiv').append(contacto);
+        $('#usersDiv').append(contacto);
 
-                $('#usersDiv')[0].scrollTop = $('#usersDiv')[0].scrollHeight;
-                idNuevo++;
-                contacto="";
-            };
+        $('#usersDiv')[0].scrollTop = $('#usersDiv')[0].scrollHeight;
+        idNuevo++;
+        contacto="";
+    };
 
-             /*funcion para cerrar sesion*/
+    /*funcion para cerrar sesion*/
     function Logout()
     {
 
