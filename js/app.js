3c0f22d1a41a328e6d2f6a0e3bf1c0118c52b623
alejandro <alejandroisel@gmail.com>
2015-06-19 09:13:55 -0500

    no notification chrome

--- js/app.js
+++ js/app.js
@@ -5,11 +5,7 @@
   var todosPAP={};
   var module = angular.module('app', ['onsen']);
 
-var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
-if(!is_chrome)
-{
-    alert("Tu navegador no es chrome te sugerimos que bajes chrome para una mejor navegación");
-}
+
   
 
   module.controller('AppController', function($scope, $data) {
