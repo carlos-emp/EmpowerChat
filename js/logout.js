b9d28f4bdca89c53a6bd91917bced9fd8809bdfb
carlos-emp <carlos.vazquez@empowerlabs.com>
2015-06-19 13:17:35 -0500

    Logout

---
+++ js/logout.js
@@ -0,0 +1,26 @@
+/*funcion para cerrar sesion*/
+    function Logout()
+    {
+
+        //elimino las cookies
+        $.session.remove("EmailUser");
+        $.session.remove("PasswordUser");
+        detectLogin();
+
+
+    }
+
+/*variables para saber si esta  o no logueado*/
+    function detectLogin(){
+        /*obtengo los valores*/
+        var email_user_=$.session.get("EmailUser");
+        var password_user_=$.session.get("PasswordUser");
+        var Object_user_=JSON.parse($.session.get("ObjectUser"));
+        if(typeof(email_user_)=='string' && typeof(password_user_)=="string")
+        {
+            $(".email-user").text(Object_user_.Name)
+
+        }else{
+            window.location="chat-login.html";
+        }
+    }
